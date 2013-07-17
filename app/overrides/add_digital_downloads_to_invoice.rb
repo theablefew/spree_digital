242d354cdae37c193c6016efd15091a22f2e1ada
John Clem <johnnyclem@gmail.com>
2013-07-17 14:39:44 -0700

    cleaned up download link on invoice page

--- app/overrides/add_digital_downloads_to_invoice.rb
+++ app/overrides/add_digital_downloads_to_invoice.rb
@@ -6,7 +6,7 @@ Deface::Override.new(:virtual_path => "spree/shared/_order_details",
   <%= content_tag(:p, :class => 'download_links') do %>
     <% item.digital_links.each do |digital_link| %>
     <% format = File.extname(digital_link.digital.attachment.path).downcase %>
-      <%= link_to t(:digital_download, :type => "application/zip"), digital_url(:host => Spree::Config.get(:site_url), :secret => digital_link.secret), :class => "btn #{format}" %>
+      <%= link_to "Click To Download Your Software", digital_url(:host => Spree::Config.get(:site_url), :secret => digital_link.secret), :class => "btn #{format}" %>
     <% end %>
   <% end %>
 <% end %>
