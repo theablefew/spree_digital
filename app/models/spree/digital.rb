a9ec1cb3b182bccee522023f7b2e17117410888c
John Clem <johnnyclem@gmail.com>
2013-07-17 15:08:11 -0700

    files will be stored in the shared folder for cap

--- app/models/spree/digital.rb
+++ app/models/spree/digital.rb
@@ -3,7 +3,7 @@ module Spree
     belongs_to :variant
     has_many :digital_links, :dependent => :destroy
   
-    has_attached_file :attachment, :path => ":rails_root/private/digitals/:id/:basename.:extension"
+    has_attached_file :attachment, :path => '/data/spree/shared/uploaded-files/digitals/:id/:basename.:extension'
   
     # TODO: Limit the attachment to one single file. Paperclip supports many by default :/
 
