7ff77a0c1c75353b66b2751db5ebf15b035e568d
John Clem <johnnyclem@gmail.com>
2013-07-19 00:42:29 -0700

    removed unnecessary index

--- db/migrate/20131207191949_add_digital_link_index_to_product_keys.rb
+++ db/migrate/20131207191949_add_digital_link_index_to_product_keys.rb
@@ -1,7 +1,5 @@
 class AddDigitalLinkIndexToProductKeys < ActiveRecord::Migration
   def change
-    add_column :spree_product_keys, :digital_link_id
-
-    add_index "spree_product_keys", ["digital_link_id"], :name => "index_product_keys_on_digital_link_id"
+    add_column :spree_product_keys, :spree_digital_link_id
   end
 end
